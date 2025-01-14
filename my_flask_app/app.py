bb6c2d61d347c6d51e0b78f569beee8647be5ec6
Podee aupavan <100249@glo.or.th>
2025-01-14 13:39:34 +0700

    .

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify, request
-from L6_Master import backendProcess
+from L6_Master import backendProcess, validateInputData
 
 app = Flask(__name__)
     
@@ -12,6 +12,7 @@ def index():
             return jsonify({"error": "Content-Type must be application/json"}), 400
 
         # อ่านข้อมูล JSON
+        selectpt = request.json.get('selectpt')
         patterns = [request.json.get(f'pattern{i}') for i in range(1, 5)]
         set_value = request.json.get('set')
         charity = request.json.get('charity')
@@ -21,9 +22,15 @@ def index():
         if not request.data:
             return jsonify({"error": "No input provided"}), 400
         
+        # validate input        
+        validate = validateInputData(selectpt,patterns,set_value,charity,lot,year)
+        status = validate[0] 
+        result_message = validate[1]
+        if status == 'Error':
+            return jsonify({"message": result_message , "status":status})
+        
         # ประมวลผลข้อมูล
-        msg = None
-        msg = backendProcess(patterns,set_value,charity,lot,year)
+        msg = backendProcess(selectpt,patterns,set_value,charity,lot,year)
         status = msg[0] 
         result_message = msg[1]    
         return jsonify({"message": result_message , "status":status})
@@ -32,5 +39,6 @@ def index():
 
 # รันเซิร์ฟเวอร์
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+    #app.run(debug=True)
     #app.run(debug=False)
